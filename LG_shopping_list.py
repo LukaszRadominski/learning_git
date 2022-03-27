a8a8d78aa0365327ff094bb3035997a825fb86e8
Łukasz Radomiński <lukaszr82@o2.pl>
2022-03-27 09:09:45 +0200

    Add loop in Init project

--- LG_shopping_list.py
+++ LG_shopping_list.py
@@ -4,3 +4,7 @@ shopping_dict = {
 }
 
 shopping_dict = {k.capitalize():[i.capitalize() for i in v] for k,v in shopping_dict.items()}
+
+for k in shopping_dict:
+    print(f"Idę do {k} i kupuję tam {shopping_dict[k]}.")
+print(f"W sumie kupuję {sum(len(shopping_dict[v]) for v in shopping_dict )} produktów")
