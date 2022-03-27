feadb022c418dc25a54c5dab9c73671f9f99c606
Łukasz Radomiński <lukaszr82@o2.pl>
2022-03-27 09:04:42 +0200

    Add capitalize in Init project

--- LG_shopping_list.py
+++ LG_shopping_list.py
@@ -2,3 +2,5 @@ shopping_dict = {
     "piekarnia": ["chleb", "bułka", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+shopping_dict = {k.capitalize():[i.capitalize() for i in v] for k,v in shopping_dict.items()}
